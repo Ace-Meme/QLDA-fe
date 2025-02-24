58b354082da0a11b836e1b638d90764490a5d43f
nhutnguyen <nguyenquocnhut11c1@gmail.com>
2025-02-24 21:33:52 +0700

    Sign up page

--- src/screens/Root.jsx
+++ src/screens/Root.jsx
@@ -1,29 +1,48 @@
-import { Link, Outlet, Route, Routes } from 'react-router'
-import { Login } from './Login'
-import { Home } from './Home'
-import { Signup } from './Signup'
-
-export function RootPath(){
-    return(
+import { Link, Outlet, Route, Routes } from "react-router";
+import { Login } from "./Login";
+import { Home } from "./Home";
+import Signup from "../screens/Signup/index";
+export function RootPath() {
+    return (
         <Routes>
-            <Route path='/' element={<RootNavigationBar />}>
-                <Route index element={<Home />} />
-                <Route path='login' element={<Login />} />
-                <Route path='signup' element={<Signup />} />
+            <Route
+                path="/"
+                element={<RootNavigationBar />}
+            >
+                <Route
+                    index
+                    element={<Home />}
+                />
+                <Route
+                    path="login"
+                    element={<Login />}
+                />
+                <Route
+                    path="signup"
+                    element={<Signup />}
+                />
             </Route>
         </Routes>
-    )
+    );
 }
 
-export function RootNavigationBar(){
-    return(
-        <div style={{width: '100vw', height: '100vh'}}>
-            <div style={{display: 'flex', flexDirection: 'row', gap: '10px', border: 'solid 1px black', padding: '5px'}}>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/login'}>Log in</Link>
-                <Link to={'/login'}>Sign Up</Link>
+export function RootNavigationBar() {
+    return (
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "10px",
+                    border: "solid 1px black",
+                    padding: "5px",
+                }}
+            >
+                <Link to={"/"}>Home</Link>
+                <Link to={"/login"}>Log in</Link>
+                <Link to={"/signup"}>Sign Up</Link>
             </div>
             <Outlet />
         </div>
-    )
+    );
 }
